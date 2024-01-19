dc299cc00fec037456cc6d4425f9d644bb2f3af2
Angelo Torella <angelotorella2002@gmail.com>
2024-01-19 08:45:05 -0400

    recibir notificacion del api y mostarar en pantalla

--- src/Controller/AlertsController.jsx
+++ src/Controller/AlertsController.jsx
@@ -1,5 +1,6 @@
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { getTokenFromMessaging } from '../Model/FirebaseConfig';
-import { showToken } from '../View/AlertsView';
 
 function requestPermission() {
   console.log("Requesting permission...");
@@ -11,15 +12,38 @@ function requestPermission() {
         .then((currentToken) => {
           if (currentToken) {
             console.log("currentToken", currentToken);
-            showToken(currentToken);
+            // Show the token as a toast notification
+            toast(`Token: ${currentToken}`, {
+              position: "top-right",
+              autoClose: 5000, // The toast will disappear after 5 seconds
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+            });
           } else {
             console.log(
               "No registration token available. Request permission to generate one."
             );
           }
         })
-    }
+    } 
   });
 }
+
+// Listen for messages from the service worker
+navigator.serviceWorker.addEventListener('message', (event) => {
+  // Show a toast with the content of the message
+  toast(event.data.notification.body, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+});
 
 export { requestPermission };
